d886056ee7fa970203415d8915b930fb4678e18e
Joshua Pena <penacalata@gmail.com>
2016-04-27 02:07:44 -0700

    fixing incorrect scale

--- dev/subject/start/start.js
+++ dev/subject/start/start.js
@@ -516,7 +516,7 @@
         return;
       }
       if ($scope.method === "BDM1" || $scope.method === "BDMWTA") {
-        $scope.actualprice = Math.floor(Math.random() * 300);
+        $scope.actualprice = Math.floor(Math.random() * 300 * scale);
         $scope.bid *= 100;
       } else if ($scope.method === "BDM2") {
         var length = $scope.bdm2values.length;
@@ -1372,7 +1372,7 @@
       if (configfile.debug[0] === "False") $scope.debug = false;
       else {
         $scope.debug = true;
-        $scope.income = $scope.incomegoal * 100 + 1;
+        $scope.income = $scope.incomegoal * 100 * $scope.scale + 1;
       }
 
       // set partner values from config file
@@ -1380,7 +1380,7 @@
       if ($scope.role !== 'R') {
         $scope.partner.index = configfile.pair[0];
         $scope.partner.role = rs.subjects[$scope.partner.index - 1].data.role[0];
-        $scope.partner.endowment = configfile.endowment[0] * 100;
+        $scope.partner.endowment = configfile.endowment[0] * 100 * scale;
         console.log("userId : " + $scope.userIndex);
         console.log("role : " + $scope.role);
         console.log("partner : " + $scope.partner);
