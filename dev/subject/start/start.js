86bc9eb2f00ff10e6ad859df292e8be7a7003ba4
Joshua Pena <penacalata@gmail.com>
2016-05-03 01:43:37 -0700

    check values

--- dev/subject/start/start.js
+++ dev/subject/start/start.js
@@ -1262,8 +1262,6 @@
           $("#finaldisgust").text(ui.value);
         }
       });
-      $(".slider1.ui-state-default").hide();
-      $(".ui-slider-labels").css("margin-left", "100%");
       // create sliders for core interaction
       if ($scope.role === "T") {
         $scope.percentSlider = $("#tSlider").labeledslider({
