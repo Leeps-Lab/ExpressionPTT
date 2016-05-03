aaf75be6a6fa336fcb9faa339acd8ece54a1ee1f
Joshua Pena <penacalata@gmail.com>
2016-05-03 01:25:59 -0700

    brackets

--- dev/subject/start/start.js
+++ dev/subject/start/start.js
@@ -1678,13 +1678,14 @@
     };
   })
   .directive('onFinishRender', function ($timeout) {
-  return {
-      restrict: 'A',
-      link: function (scope, element, attr) {
-          if (scope.$last === true) {
-              $timeout(function () {
-                  scope.$emit('ngRepeatFinished');
-              });
-          }
-      }
-  };
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            if (scope.$last === true) {
+                $timeout(function () {
+                    scope.$emit('ngRepeatFinished');
+                });
+            }
+        }
+    }
+  });
